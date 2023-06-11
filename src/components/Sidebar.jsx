60c9f2e75abb9a8d96970a197ca0abac1a028e49
parbat <parbat.chhetri79@gmail.com>
2023-06-11 12:37:45 +0545

    add

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import { AiFillHome,AiOutlineLike } from "react-icons/ai";
+import {MdSubscriptions,MdVideoLibrary,MdOutlineWatchLater} from "react-icons/md";
+import {RiHistoryLine,RiVideoLine} from "react-icons/ri";
+import {BsChevronDown} from "react-icons/bs"
 const Sidebar = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <div className="flex items-center ml-5 gap-7">
+        <AiFillHome className="text-2xl" />
+        <h1 className="text-base font-semibold">Home</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <AiFillHome className="text-2xl" />
+        <h1 className="text-base font-semibold">Shorts</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <MdSubscriptions className="text-2xl" />
+        <h1 className="text-base font-semibold">Subscriptions</h1>
+      </div>
+      <hr className="w-[230px] my-5 ml-3 border-[1px]"></hr>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <MdVideoLibrary className="text-2xl" />
+        <h1 className="text-base font-semibold">Library</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <RiHistoryLine className="text-2xl" />
+        <h1 className="text-base font-semibold">History</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <RiVideoLine className="text-2xl" />
+        <h1 className="text-base font-semibold">Your videos</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <MdOutlineWatchLater className="text-2xl" />
+        <h1 className="text-base font-semibold">Watch later</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <AiOutlineLike className="text-2xl" />
+        <h1 className="text-base font-semibold">Liked Videos</h1>
+      </div>
+      <div className="flex items-center ml-5 gap-7 mt-5">
+        <BsChevronDown className="text-2xl" />
+        <h1 className="text-base font-semibold">Show more</h1>
+      </div>
+      <hr className='w-[230px] my-5 ml-3 border-[1px]'></hr>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
