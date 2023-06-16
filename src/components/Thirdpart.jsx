4097d7ff9d9d2e82ea81c90825144c149611e395
parbat <parbat.chhetri79@gmail.com>
2023-06-16 15:42:20 +0545

    add

--- src/components/Thirdpart.jsx
+++ src/components/Thirdpart.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import { FiBellOff } from "react-icons/fi";
-import { BsChevronDown,BsThreeDots } from "react-icons/bs";
-import { AiOutlineLike, AiOutlineDislike,AiOutlineScissor } from "react-icons/ai";
-import {RiShareForwardLine} from "react-icons/ri"
-import {MdPlaylistAdd} from "react-icons/md"
+import { BsChevronDown, BsThreeDots } from "react-icons/bs";
+import {BiTimeFive} from "react-icons/bi";
+import {
+  AiOutlineLike,
+  AiOutlineDislike,
+  AiOutlineScissor,
+} from "react-icons/ai";
+import { RiShareForwardLine,RiMenuUnfoldFill } from "react-icons/ri";
+import { MdPlaylistAdd } from "react-icons/md";
 import photo3 from "../assests/photo3.jpg";
 import Data1 from "./Data1";
 import { Link } from "react-router-dom";
@@ -13,8 +18,8 @@ export const Thirdpart = () => {
     <div>
       <Header />
       <div className="flex">
-      <Video />
-      <Sidepart />
+        <Video />
+        <Sidepart />
       </div>
     </div>
   );
@@ -24,29 +29,39 @@ function Video() {
   return (
     <div>
       <div className="ml-20 flex flex-col gap-3">
-        <div className="w-[1000px] h-[550px] bg-red-600 "></div>
+        <div className="w-[950px] h-[550px] bg-red-600 ">
+          <div className="">
+            <iframe
+              className="w-full  h-[550px]"
+              src={`https://www.youtube.com/embed/F3OP1hduA-s`}
+              title="YouTube Video Player"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
         <h1 className="font-semibold text-lg">
           Hello Welcome to my Vlog | Flying Beast | Enjoy Guys!
         </h1>
 
-        <div className="flex gap-48">
-            <div className="flex gap-3 cursor-pointer">
-          <img
-            src={photo3}
-            alt="pp"
-            className="rounded-full w-[35px] h-[35px]  "
-          />
-          <div>
-            <h1 className="font-bold text-sm"> Flying Beast </h1>
-            <p className="font-normal text-xs"> 15M subscribers </p>
-          </div>
-          <div className="flex ">
-            <div className="w-[150px] h-[38px] rounded-3xl bg-gray-100 hover:bg-gray-300 cursor-pointer flex items-center gap-2 justify-center ml-5">
-              <FiBellOff className="text-lg" />
-              <p className="font-bold text-sm">Subscribed</p>
-              <BsChevronDown className="text-lg mt-1 " />
+        <div className="flex gap-36">
+          <div className="flex gap-3 cursor-pointer">
+            <img
+              src={photo3}
+              alt="pp"
+              className="rounded-full w-[35px] h-[35px]  "
+            />
+            <div>
+              <h1 className="font-bold text-sm"> Flying Beast </h1>
+              <p className="font-normal text-xs"> 15M subscribers </p>
             </div>
-          </div>
+            <div className="flex ">
+              <div className="w-[150px] h-[38px] rounded-3xl bg-gray-100 hover:bg-gray-300 cursor-pointer flex items-center gap-2 justify-center ml-5">
+                <FiBellOff className="text-lg" />
+                <p className="font-bold text-sm">Subscribed</p>
+                <BsChevronDown className="text-lg mt-1 " />
+              </div>
+            </div>
           </div>
 
           <div className="flex">
@@ -78,29 +93,86 @@ function Video() {
   );
 }
 
-function Sidepart(){
-    return(
-        <div>
-           {Data1.map((item, index) => (
-           <Link to='/playvideo'>
-          <div key={index} className="shrink-0 w-[400px] h-[180px] bg-gray-100 rounded-xl mt-4 flex ml-5   hover:scale-105 cursor-pointer hover:bg-slate-300 duration-200">
-            <div>
-              <img src={item.image} alt="pp" className="shrink-0 w-[350px] h-[180px] rounded-xl " />
-            </div>
-            <div className='mt-3 ml-3'>
-                <p className='font-semibold text-base'>{item.title}|{item.title1}|{item.id}</p>
-                <p className='font-bold '>{item.Views}{" views  "}.{" "}{item.time}</p>
-                <div className='flex items-center shrink-0 object-contain mt-3 gap-2  '>
-                    <img src={item.image} alt='pp' className='w-[25px] h-[25px] rounded-full'/>
-                    <h1 className='font-semibold text-sm'>{item.id}</h1>
-                     </div>
-                     <p className='text-xs font-sans mt-3'>{item.desc}</p>
-                </div>
-               
-           
-          </div>
-          </Link>
-        ))}  
+function Sidepart() {
+  return (
+    <div className="ml-3 ">
+
+<div class="flex snap-x snap-proximity  overflow-auto scrollbar-hidden scroll-smooth w-[400px]">
+      <div className="flex gap-2 ml-1 mt-2 ">
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">All</h1>
         </div>
-    )
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Gaming</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Music</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Videos</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">News</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Mixes</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Comedy</h1>
+        </div>
+        <div className="shrink-0 w-[120px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Pubg Mobile</h1>
+        </div>
+        <div className="shrink-0 w-[90px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Movies</h1>
+        </div>
+        <div className="shrink-0 w-[120px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Entertainment</h1>
+        </div>
+        <div className="shrink-0 w-[120px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Electronics</h1>
+        </div>
+        <div className="shrink-0 w-[140px] h-[30px] rounded-lg bg-gray-400 flex justify-center items-center cursor-pointer hover:bg-slate-400">
+          <h1 className="text-base font-medium">Sports and Games</h1>
+        </div>
+        </div>
+        </div>
+
+        {Data1.map((item, index) => (
+  <Link to="/playvideo">
+    <div
+      key={index}
+      className="w-[420px] h-[110px] bg-gray-100 rounded-xl mt-4 flex ml-1 cursor-pointer relative group"
+    >
+      <img
+        src={item.image}
+        alt="pp"
+        className="object-cover min-w-[180px] h-[110px] rounded-xl"
+      />
+
+      <div className="bg-black w-7 h-7 absolute left-36 z-10 top-1 rounded-md flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+        <BiTimeFive className="text-white text-2xl" />
+      </div>
+
+      <div className="bg-black w-7 h-7 absolute left-36 z-10 top-9 rounded-md flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+        <RiMenuUnfoldFill className="text-white text-2xl" />
+      </div>
+
+      <div className="mt-1 ml-3">
+        <p className="font-semibold text-base">
+          {item.title}|{item.title1}|{item.id}
+        </p>
+        <h1 className="font-semibold text-sm">{item.id}</h1>
+        <p className="font-medium">
+          {item.Views} views. {item.time}
+        </p>
+      </div>
+    </div>
+  </Link>
+))}
+
+    
+    </div>
+ 
+  );
 }
